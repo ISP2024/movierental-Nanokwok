66f3e01d9c417ea95de43d94b9fe2ceebbda569f
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-10-20 15:27:24 +0700

    Add these useful attributes and methods

--- movie.py
+++ movie.py
@@ -1,3 +1,5 @@
+from typing import Collection
+
 from pricing import RegularPrice, NewReleasePrice, ChildrenPrice
 
 
@@ -10,11 +12,16 @@ class Movie:
     NEW_RELEASE = NewReleasePrice()
     CHILDRENS = ChildrenPrice()
 
-    def __init__(self, title):
+    def __init__(self, title: str, year: int, genre: Collection[str]):
         self.title = title
+        self.year = year
+        self.genres = genre
 
     def get_title(self):
         return self.title
 
+    def is_genre(self, genre: str) -> bool:
+        return genre.lower() in self.genres
+
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.year})"
