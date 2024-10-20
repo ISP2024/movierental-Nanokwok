c75e10dce93dafefcce4b9c7d915304a613f9d66
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-10-20 17:21:21 +0700

    Define a Factory for Movies

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 # Demonstrate the movie rental code.
 # Create a customer with some movies and print a statement.
 
-from movie import Movie
+from movie import Movie, MovieCatalog
 from rental import Rental
 from customer import Customer
 
 def make_movies():
     """Some sample movies."""
+    moviecatalog = MovieCatalog()
     movies = [
-        Movie("Air", Movie.NEW_RELEASE),
-        Movie("Oppenheimer", Movie.REGULAR),
-        Movie("Frozen", Movie.CHILDRENS),
-        Movie("Bitconned", Movie.NEW_RELEASE),
-        Movie("Particle Fever", Movie.REGULAR)
+        # Movie("Air"),
+        # Movie("Oppenheimer"),
+        # Movie("Frozen"),
+        # Movie("Bitconned"),
+        # Movie("Particle Fever")
+        moviecatalog.get_movie("Air")
     ]
     return movies
 
 
 if __name__ == '__main__':
-    # Create a customer with some rentals
-    customer = Customer("Edward Snowden")
-    days = 1
-    for movie in make_movies():
-        customer.add_rental(Rental(movie, days, movie.price_strategy))
-        days = (days + 2) % 5 + 1
-    print(customer.statement())
+    # # Create a customer with some rentals
+    # customer = Customer("Edward Snowden")
+    # days = 1
+    # for movie in make_movies():
+    #     customer.add_rental(Rental(movie, days, movie.price_strategy))
+    #     days = (days + 2) % 5 + 1
+    # print(customer.statement())
+    # Get the Singleton Movie Catalog
+    catalog = MovieCatalog()
+    # Get the first movie named 'Mulan'
+    movie = catalog.get_movie("Mulan")
+    # Get 'Mulan' released in 1998
+    old_movie = catalog.get_movie("Mulan", 1998)
+    print(movie)
+    print(old_movie)
--- movie.py
+++ movie.py
@@ -1,25 +1,77 @@
 from typing import Collection
-
 from pricing import PriceStrategy
 from dataclasses import dataclass
-from typing import Collection
-
+import csv
 
 @dataclass(frozen=True)
 class Movie:
     """
     A movie available for rent.
     """
+
     title: str
-    price_strategy: PriceStrategy
     year: int
     genres: Collection[str]
 
-    def get_title(self):
+    def get_title(self) -> str:
+        """
+        Retrieve the title of the movie.
+        """
         return self.title
 
-    def is_genre(self, genre: str) -> bool:
-        return genre.lower() in self.genres
+    def is_genre(self, string: str) -> bool:
+        """
+        Check if the movie belongs to a specific genre.
+        """
+        return string.lower() in self.genres
 
-    def __str__(self):
-        return f"{self.title} ({self.year})"
+    def __str__(self) -> str:
+        """
+        String representation of the Movie instance.
+        """
+        return f" {self.title} ({self.year})"
+    
+class MovieCatalog:
+    """
+    A singleton class that manages a catalog of movies.
+    """
+
+    __instances = None
+
+    def __new__(cls):
+        """
+        Create a new instance of MovieCatalog or return the existing instance.
+        """
+        if cls.__instances is None:
+            cls.__instances = super(MovieCatalog, cls).__new__(cls)
+            cls.__instances._movie_list = cls.__load_data()
+        return cls.__instances
+
+    @staticmethod
+    def __load_data():
+        """
+        Load movie data from a CSV file and create Movie instances.
+        """
+        movies = []
+        with open("movies.csv", encoding="utf-8") as movie:
+            reader = csv.DictReader(movie)
+            next(reader)
+            for movie_, movie_data in enumerate(reader):
+                try:
+                    movie = Movie(
+                        movie_data['title'],
+                        int(movie_data['year']),
+                        movie_data['genres'].split('|'))
+                    movies.append(movie)
+                except (TypeError, ValueError):
+                    continue
+        return movies
+
+    def get_movie(self, title, year=None):
+        """
+        Retrieve a movie by its title and optional release year.
+        """
+        for movie in self._movie_list:
+            if title.lower() == movie.title.lower() and (not year or year == movie.year):
+                return movie
+        return None
